14254a24f8a2e382d1aacbefa427b314ca6fe559
J Saravana <83276590+saravanaCr7@users.noreply.github.com>
2021-04-27 14:39:25 +0530

    added

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ProductDataAccessLayer;
+using ProductBusinessLogicLayer;
+
+namespace ProductPresentatoinLayer
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ProductLogic logic = new ProductLogic();
+            do
+            {
+                Console.WriteLine("1 insert into product \n  2 insert into category \n 3 retrive data from category\n  4 retrive data from product \n 5 retrive products basedon id \n 6 update product price based on id \n 7 delete product based on id");
+                Console.WriteLine("select your choice");
+                
+                int choice = int.Parse(Console.ReadLine());
+                logic.Selectchoice(choice);
+
+
+
+
+            } while (true);
+        }
+    }
+}
